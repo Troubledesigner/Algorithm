e40a5f29bf99e99ef652d45542f5f4f7c7554441
Troubledesigner <821878167@qq.com>
2018-10-30 16:13:35 +0800

    Initial Commit

---
+++ Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by Walker on 2018/10/30.
+//  Copyright © 2018 Hust.cs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
